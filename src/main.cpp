dfb0a94ee982007af916abe08f37a958267a18ec
SizzlinSeal <eduard.teale@gmail.com>
2021-03-25 10:35:17 -0700

    start brain screen work

--- src/main.cpp
+++ src/main.cpp
@@ -706,6 +706,40 @@ void usercontrol(){
   }
 }
 
+double drivetrainTemp() {
+  return (leftMotorA.temperature() + leftMotorB.temperature() + rightMotorA.temperature() + rightMotorB.temperature()) / 4;
+}
+
+int brainScreen() {
+  // draw boxes here so we dont waste cpu resources
+  Brain.Screen.setPenColor(white);
+  Brain.Screen.setFillColor(transparent);
+  Brain.Screen.drawRectangle(0, 110, 200, 110);
+  Brain.Screen.drawRectangle(210, 110, 210, 110);
+  Brain.Screen.drawImageFromFile("image.png", 200, 180);
+  Brain.Screen.setFont(propXXL);
+  Brain.Screen.setPenColor(green);
+  Brain.Screen.print("10012W");
+  Brain.Screen.setPenColor(white);
+
+  for(;;) {
+    Brain.Screen.setFont(monoL);
+    
+    // display text in the boxes
+    Brain.Screen.printAt(20, 150, "Battery:");
+    Brain.Screen.printAt(20, 200, "%d percent", Brain.Battery.capacity());
+    Brain.Screen.printAt(250, 175, "Time: %d ", drivetrainTemp());
+    // sleep 1000 msecs
+    this_thread::sleep_for(1000);
+    // clear the line
+    Brain.Screen.clearLine(20, 200);
+    Brain.Screen.clearLine(250, 175);
+    // rumble to signal end of match coming
+
+  }
+  return 0;
+}
+
 void pre_auton(){
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
