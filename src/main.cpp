eaa3d8062504ac9f99681f5d40842b30f1e64434
SizzlinSeal <eduard.teale@gmail.com>
2021-03-29 11:48:36 -0700

    fixed Liam's controls

--- src/main.cpp
+++ src/main.cpp
@@ -1029,110 +1029,59 @@ void usercontrol(){
   }
 }
 
+void liamButtonL1() {
+  TopIndexer.spin(reverse, 400, rpm);
+  BottomIndexer.spin(reverse, 400, rpm);
+  waitUntil(!Controller1.ButtonL1.pressing());
+  TopIndexer.stop();
+  BottomIndexer.stop();
+}
+
+void liamButtonL2() {
+  TopIndexer.spin(forward, 600, rpm);
+  BottomIndexer.spin(forward, 600, rpm);
+  waitUntil(!Controller1.ButtonL2.pressing());
+  TopIndexer.stop();
+  BottomIndexer.stop();
+}
+
+void liamButtonR1() {
+  IntakeL.spin(reverse, 600, rpm);
+  IntakeR.spin(reverse, 600, rpm);
+  waitUntil(!Controller1.ButtonR1.pressing());
+  IntakeL.stop();
+  IntakeR.stop();
+}
+
+void liamButtonR2() {
+  IntakeL.spin(forward, 600, rpm);
+  IntakeR.spin(forward, 600, rpm);
+  waitUntil(!Controller1.ButtonR2.pressing());
+  IntakeL.stop();
+  IntakeR.stop();
+}
+
+
 
 // Liam's drivercontrol code
-void usercontrol2(){
+void usercontrol2() {
+  Controller1.ButtonL1.pressed(liamButtonL1);
+  Controller1.ButtonL2.pressed(liamButtonL2);
+  Controller1.ButtonR1.pressed(liamButtonR1);
+  Controller1.ButtonR2.pressed(liamButtonR2);
 
-  bool Donald = false;
-  int deadband = 0;
-  while (1) {
-    Brain.Screen.newLine();
-    double drive = 1;
-    /*int Ch1 = Controller1.Axis1.value();
-    int Ch3 = Controller1.Axis3.value();
-    int Ch4 = Controller1.Axis4.value();
-    Brain.Screen.print(Ch4);
-    LeftMotorF.spin(directionType::fwd, Ch3 - Ch1 - Ch4,velocityUnits::pct);
-    LeftMotorB.spin(directionType::fwd, Ch3 + Ch1 - Ch4,velocityUnits::pct);
-    RightMotorF.spin(directionType::fwd, Ch3 + Ch1 + Ch4,velocityUnits::pct);
-    RightMotorB.spin(directionType::fwd, Ch3 - Ch1 + Ch4,velocityUnits::pct);*/
-    //Get the velocity percentage of the left motor. (Axis3 + Axis1)
-    int leftMotorSpeed =
-        Controller1.Axis3.position() + Controller1.Axis1.position() * drive;
-    // Get the velocity percentage of the right motor. (Axis3 - Axis1)
-    int rightMotorSpeed =
-        Controller1.Axis3.position() - Controller1.Axis1.position() * drive;
-    // Set the speed of the left motor. If the value is less than the deadband,
-    // set it to zero.
-    if (Controller1.ButtonUp.pressing()){
-      drive = 1.0;
-    
-    }
-    if (Controller1.ButtonDown.pressing()){
-      drive = 0.5;
-    }
-    if (Controller1.ButtonL1.pressing()) {
-      Donald = true;
-    }
-    if (Controller1.ButtonUp.pressing()) {
-      Donald = false;
-    }
-      if (abs(leftMotorSpeed) < deadband) {
-      // Set the speed to zero.
-      LF.setVelocity(0, percent);
-      LB.setVelocity(0, percent);
-    } else {
-      // Set the speed to leftMotorSpeed
-      LF.setVelocity(leftMotorSpeed, percent);
-      LB.setVelocity(leftMotorSpeed, percent);
-    }
-    // Set the speed of the right motor. If the value is less than the deadband,
-    // set it to zero.
-    if (abs(rightMotorSpeed) < deadband) {
-      // Set the speed to zero
-      RF.setVelocity(0, percent);
-      RB.setVelocity(0, percent);
-    } else {
-      // Set the speed to rightMotorSpeed
-      RF.setVelocity(rightMotorSpeed, percent);
-      RB.setVelocity(rightMotorSpeed, percent);
-    }
-    // Spin both motors in the forward direction.
-    LF.spin(forward);
-    LB.spin(forward);
-    RF.spin(forward);
-    RB.spin(forward);
-    wait(25, msec);
-    // Tank Drive Code
-    if (Donald == true) {
-      IntakeR.spin(directionType::rev, 140, vex::velocityUnits::pct);
-      IntakeL.spin(directionType::rev, 140, vex::velocityUnits::pct);
-      BottomIndexer.spin(directionType::rev, 140, vex::velocityUnits::pct);
-    } else if (Donald == false) {
-      IntakeR.stop();
-      IntakeL.stop();
-      BottomIndexer.stop();
-    }
-    if (Controller1.ButtonR1.pressing()) {
-/*      TopIndexer.spin(directionType::rev, 140, vex::velocityUnits::pct);
-      BottomIndexer.spin(directionType::fwd, 140, vex::velocityUnits::pct);
-      IntakeR.spin(directionType::fwd, 127, vex::velocityUnits::pct);
-      IntakeL.spin(directionType::fwd, 127, vex::velocityUnits::pct);*/
-      IntakeL.spin(reverse, 140, percent);
-      IntakeR.spin(reverse, 140, percent);
-    } else if (Controller1.ButtonR2.pressing()) {
-/*       TopIndexer.spin(directionType::fwd, 140, vex::velocityUnits::pct);
-      BottomIndexer.spin(directionType::rev, 140, vex::velocityUnits::pct);*/
-      IntakeL.spin(forward, 140, percent);
-      IntakeR.spin(forward, 140, percent);
-    } else if (Controller1.ButtonL2.pressing()) {
-/*      IntakeL.spin(directionType::rev, 127, vex::velocityUnits::pct);
-      IntakeR.spin(directionType::rev, 127, vex::velocityUnits::pct);
-      BottomIndexer.spin(directionType::rev, 140, vex::velocityUnits::pct); */
-            TopIndexer.spin(reverse, 140, percent);
-      BottomIndexer.spin(reverse, 140, percent);
-    } else if (Controller1.ButtonL1.pressing()) {
-/*      IntakeR.spin(directionType::fwd, 140, vex::velocityUnits::pct);
-      IntakeL.spin(directionType::fwd, 140, vex::velocityUnits::pct);
-      BottomIndexer.spin(directionType::fwd, 140, vex::velocityUnits::pct);*/
-            TopIndexer.spin(forward, 140, percent);
-      BottomIndexer.spin(forward, 140, percent);
-    } else {
-      TopIndexer.stop();
-      BottomIndexer.stop();
-      IntakeL.stop();
-      IntakeR.stop();
-    }
+  while(true) {
+	// arcade control
+  int drivetrainLeftSideSpeed = Controller1.Axis3.position() + Controller1.Axis1.position();
+  int drivetrainRightSideSpeed = Controller1.Axis3.position() - Controller1.Axis1.position();
+  
+
+	LB.spin(forward, drivetrainLeftSideSpeed, percent);
+  LF.spin(forward, drivetrainLeftSideSpeed, percent);
+  RB.spin(forward, drivetrainRightSideSpeed, percent);
+  RB.spin(forward, drivetrainRightSideSpeed, percent);
+
+	vexDelay(5);
   }
 }
 
