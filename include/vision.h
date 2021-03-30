5588a3ade6787aa23aa48770a3a2c7b6444786bc
SizzlinSeal <eduard.teale@gmail.com>
2021-03-30 14:29:30 -0700

    tune for goals

--- include/vision.h
+++ include/vision.h
@@ -2,10 +2,10 @@
 #include "vex.h"
 vex::vision::signature RED_BALL = vex::vision::signature (1, 7957, 11989, 9973, -1567, -227, -897, 3.1, 0);
 vex::vision::signature BLUE_BALL = vex::vision::signature (2, 0, 0, 0, 0, 0, 0, 3, 0);
-vex::vision::signature GOAL_CORNER = vex::vision::signature (3, 0, 0, 0, 0, 0, 0, 3, 0);
+vex::vision::signature GOAL = vex::vision::signature (3, -4681, -3383, -4032, -5691, -3787, -4739, 2.6, 0);
 vex::vision::signature PURE_RED = vex::vision::signature (4, 0, 0, 0, 0, 0, 0, 2.5, 0);
 vex::vision::signature SIG_5 = vex::vision::signature (5, 0, 0, 0, 0, 0, 0, 2.5, 0);
 vex::vision::signature SIG_6 = vex::vision::signature (6, 0, 0, 0, 0, 0, 0, 2.5, 0);
 vex::vision::signature SIG_7 = vex::vision::signature (7, 0, 0, 0, 0, 0, 0, 2.5, 0);
-vex::vision Vision1 = vex::vision (vex::PORT16, 50, RED_BALL, BLUE_BALL, GOAL_CORNER, PURE_RED, SIG_5, SIG_6, SIG_7);
+vex::vision Vision1 = vex::vision (vex::PORT16, 50, RED_BALL, BLUE_BALL, GOAL, PURE_RED, SIG_5, SIG_6, SIG_7);
 /*vex-vision-config:end*/
